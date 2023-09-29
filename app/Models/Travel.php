814e1a33040cfd8bc1f7d817d0782c14d38c1317
Mikolaj Lysakowski <7057659u@zstzbaszynek.pl>
2023-09-29 16:01:01 +0200

    wip

--- app/Models/Travel.php
+++ app/Models/Travel.php
@@ -2,11 +2,28 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Travel extends Model
 {
     use HasFactory;
     protected $table = 'travels';
+
+public function tours(): HasMany{
+    return $this->hasMany(tour::class);
+}
+
+public function numberOfNights(): Attribute
+{
+return Attribute::make(
+        get: fn ($value, $attributes)=>$attributes['number_of_days']-1
+);
+}
+
+
+
 }
